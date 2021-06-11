808392c396ae92565b84106832c6818f543dce83
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-06-11 01:43:59 -0300

    init

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// igual a 'function Dashboard(){}
+const Dashboard: React.FC = () => {
+  return <h1>Explore repositórios no Github</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// igual a 'function Repository(){}
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
